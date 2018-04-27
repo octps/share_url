43a77262f9f86072575debbc936c26e2b11244a1
octps <renmo9@gmail.com>
2018-04-28 00:43:32 +0900

    #3 トップの概略レイアウト

---
+++ index.php
@@ -0,0 +1,115 @@
+<?php
+
+?>
+<!doctype html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, maximum-scale=1.0, minimum-scale=0.5,user-scalable=yes,initial-scale=1.0" />
+    <meta name="description" content="puprlはwebでブックマークするサービスです。">
+    <meta name="author" content="">
+    <link rel="icon" href="/images/favicon.ico">
+
+    <!-- css framework読み込み（スタイリングのため） -->
+    <title>puprl | webでブックマークするサービス</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.0.3/milligram.min.css">
+    <link rel="stylesheet" href="/css/index.css">
+  </head>
+
+  <body>
+    <div class="">
+      <header class="row in-center">
+        <div class="column header t-center"><h1>puprl</h1></div>
+        <div class=""></div>
+      </header>
+      <div class="main container contents">
+        <div>
+          <p  class="twitter_login t-center"><a class="button button-small" href="/twitterlogin.php">twitter login</a></p>
+        </div>
+        <div class="main container">
+          <div class="bookmark">
+            <div class="titles row">
+                <div class="title column column-75">
+                  <h2><a href="">タイトル</a></h2>
+                  <p><a href="" target="_blank">http://google.com</a></p>
+                </div>
+                <div class="column column-20">
+                  <img src="/images/sample.jpg">
+                </div>
+            </div>
+            <div class="row">
+            </div>
+            <div class="comments">
+              <p><a href="">ユーザー名</a>ああああああ</p>
+              <p><a href="">ユーザー名</a>いいい、ああ、うう。</p>
+              <p><a href="">ユーザー名</a>ああああああはしししはちはちとしは</p>
+            </div>
+          </div>
+        </div>
+        <div class="main container">
+          <div class="bookmark">
+            <div class="titles row">
+                <div class="title column column-75">
+                  <h2><a href="">タイトル</a></h2>
+                  <p><a href="" target="_blank">http://google.com</a></p>
+                </div>
+                <div class="column column-20">
+                  <img src="/images/sample.jpg">
+                </div>
+            </div>
+            <div class="row">
+            </div>
+            <div class="comments">
+              <p><a href="">ユーザー名</a>ああああああ</p>
+              <p><a href="">ユーザー名</a>いいい、ああ、うう。</p>
+              <p><a href="">ユーザー名</a>ああああああはしししはちはちとしは</p>
+            </div>
+          </div>
+        </div>
+        <div class="main container">
+          <div class="bookmark">
+            <div class="titles row">
+                <div class="title column column-75">
+                  <h2><a href="">タイトル</a></h2>
+                  <p><a href="" target="_blank">http://google.com</a></p>
+                </div>
+                <div class="column column-20">
+                  <img src="/images/sample.jpg">
+                </div>
+            </div>
+            <div class="row">
+            </div>
+            <div class="comments">
+              <p><a href="">ユーザー名</a>ああああああ</p>
+              <p><a href="">ユーザー名</a>いいい、ああ、うう。</p>
+              <p><a href="">ユーザー名</a>ああああああはしししはちはちとしは</p>
+            </div>
+          </div>
+        </div>
+        <div class="main container">
+          <div class="bookmark">
+            <div class="titles row">
+                <div class="title column column-75">
+                  <h2><a href="">タイトル</a></h2>
+                  <p><a href="" target="_blank">http://google.com</a></p>
+                </div>
+                <div class="column column-20">
+                  <img src="/images/sample.jpg">
+                </div>
+            </div>
+            <div class="row">
+            </div>
+            <div class="comments">
+              <p><a href="">ユーザー名</a>ああああああ</p>
+              <p><a href="">ユーザー名</a>いいい、ああ、うう。</p>
+              <p><a href="">ユーザー名</a>ああああああはしししはちはちとしは</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <footer class="row in-center">
+        <div class="column footer t-center">footer</div>
+      </footer>
+    </div>
+  </body>
+</html>
